6202bd885ea934fd42b8176654e77f7ec7218082
Ivan-Rdgz <ivan13516@outlook.com>
2026-05-07 17:06:36 -0600

    .

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ import cv2
 import numpy as np
 from scipy import signal
 import matplotlib.pyplot as plt
+from cell_counter.preprocesar import convert2gray, transf_intensity, fourier_lowPass, filter_sobel
+
+img = cv2.imread("monedas.jpg")
+
+
+
