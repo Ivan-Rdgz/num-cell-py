f72ad2f9d35814f1107b350448476e272a3b54a9
Ivan-Rdgz <ivan13516@outlook.com>
2026-05-08 16:35:57 -0600

    .

--- cell_counter/__init__.py
+++ cell_counter/__init__.py
@@ -6,10 +6,7 @@ from .preprocesar import filtro_pasa_bajas, intensidad_gamma, filter_sobel
 from .segmentacion import umbralizar, segmentar, componentes_conectados
 
 def count_cells(img_rgb, gamma=1.2, radio=30):
-    """
-    Función principal para el conteo de células.
-    Realiza preprocesamiento, umbralización y segmentación.
-    """
+    """Funcion principal: Conteo de células, se necesita una imagen RGB"""
     img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2GRAY)
     img1 = filtro_pasa_bajas(img_gray, radio=radio)
     img2 = intensidad_gamma(img1, gamma=gamma)
