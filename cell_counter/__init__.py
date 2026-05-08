8314a689367024b476662234104227725412a3f7
Ivan-Rdgz <ivan13516@outlook.com>
2026-05-08 16:33:24 -0600

    .

--- cell_counter/__init__.py
+++ cell_counter/__init__.py
@@ -1,12 +1,15 @@
+import cv2
 from . import data
 from . import preprocesar
 from . import segmentacion
-import cv2
-from .data import sangre, protoplastos, chlorella_1, chlorella_2, animal, onion_1, onion_2, onion_3, monocytes, mesothelium, onion_4, onion_5, tissue, stem
 from .preprocesar import filtro_pasa_bajas, intensidad_gamma, filter_sobel
 from .segmentacion import umbralizar, segmentar, componentes_conectados
 
 def count_cells(img_rgb, gamma=1.2, radio=30):
+    """
+    Función principal para el conteo de células.
+    Realiza preprocesamiento, umbralización y segmentación.
+    """
     img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2GRAY)
     img1 = filtro_pasa_bajas(img_gray, radio=radio)
     img2 = intensidad_gamma(img1, gamma=gamma)
