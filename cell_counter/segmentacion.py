9a9303c59e5b24f7c00da7b54e54130aa2de2502
Ivan-Rdgz <ivan13516@outlook.com>
2026-05-08 16:18:18 -0600

    .

--- cell_counter/segmentacion.py
+++ cell_counter/segmentacion.py
@@ -12,30 +12,35 @@ def umbralizar(img):
     return binaria
 
 def segmentar(img):
-    filtro_limpieza = np.array([[1, 1, 1],
-                           [1, 1, 1],
-                           [1, 1, 1]], dtype=np.float32) / 9
-
-    # Pasamos el filtro 2 veces para limpiar el ruido (como iterations en morphology)
+    # Usamos un kernel un poco más grande para cerrar huecos dentro de los núcleos
+    filtro_limpieza = np.ones((5, 5), dtype=np.float32) / 25
+    
+    # Aplicamos una vez para suavizar
     limpia = signal.convolve2d(img, filtro_limpieza, mode='same')
-    limpia = signal.convolve2d(limpia, filtro_limpieza, mode='same')
-
+    
+    # Umbralizamos para recuperar la binaria
     binaria_final = np.zeros(limpia.shape, dtype=np.uint8)
-    binaria_final[limpia > 127] = 255
+    binaria_final[limpia > 150] = 255 # Subimos un poco el umbral de limpieza
     return binaria_final
 
-def componentes_conectados(img,img_gray):
+def componentes_conectados(img, img_gray):
     num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(img, 8)
     resultado_vis = cv2.cvtColor(img_gray, cv2.COLOR_GRAY2BGR)
     conteo_final = 0
-    for i in range(1, num_labels): 
-   
+    
+    alto_max, ancho_max = img.shape[:2]
+
+    for i in range(1, num_labels):
         x, y, w, h, area = stats[i]
         
-        if area > 150:
-            conteo_final += 1
-            cv2.rectangle(resultado_vis, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            cv2.putText(resultado_vis, str(conteo_final), (x, y-5), 
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+        # --- FILTRO INTELIGENTE ---
+        # 1. El área debe ser razonable (ni muy pequeña ni gigante)
+        # 2. El ancho y alto no pueden ser más de la mitad de la imagen (adiós líneas largas)
+        if 200 < area < (alto_max * ancho_max * 0.1): 
+            if w < (ancho_max * 0.5) and h < (alto_max * 0.5):
+                conteo_final += 1
+                cv2.rectangle(resultado_vis, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                cv2.putText(resultado_vis, str(conteo_final), (x, y-5), 
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255, 0, 0), 1)
+                            
     return resultado_vis, conteo_final
-
